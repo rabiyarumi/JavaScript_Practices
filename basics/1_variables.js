18815d9e7ae087bc7c72e10d5d7cd5df14de9c15
Rabiya Rumi <rabiyaarumi@gmail.com>
2025-03-02 17:48:00 +0000

    practice declaring and using variables

--- basics/1_variables.js
+++ basics/1_variables.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const name = "Sidra" //block scope and can not redeclare and reasigned
+let age = 6; // block scope and can not redeclare but reassigned
+var country = "Bangladesh"; // function scope can be rediclared and reassigned  // Prefer not to use
+currentCity = "Qatar" // no scope can be redrclared and reassigned // never ever use it
+let nationality; // value will be undifined
+
+
+console.table([name, age, country,currentCity, nationality])
